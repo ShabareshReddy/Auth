d77dcf32aee3f1cb8a3b66e5b038541791f71e4b
ShabareshReddy <chandrareddy85580@gmail.com>
2025-08-22 20:47:04 +0530

    changing the URLs

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.cors_origin,
+    origin: process.env.FRONTENT_URL,
     credentials: true
 }));
 

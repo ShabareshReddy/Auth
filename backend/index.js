b6685e96b22a00d952c36ba9e2077248e6256f5d
ShabareshReddy <chandrareddy85580@gmail.com>
2025-08-22 21:45:24 +0530

    methods initailizes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
 

f28f6810471daf08b0ec2f8c3bf51c1005c4043f
ShabareshReddy <chandrareddy85580@gmail.com>
2025-08-22 21:56:47 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const connectDb=require("./db");
 dotenv.config();
 const app=express();
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
